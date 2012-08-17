a045cea4d12022862d6233456cbeda9273687a86
Steve Ross <sxross@gmail.com>
2012-08-17 10:40:42 -0700

    Made modifications to allow tests to pass taking into account path changes made in previous commit.

--- Rakefile
+++ Rakefile
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
 $:.unshift("/Library/RubyMotion/lib")
 require 'motion/project'
-# require 'rubygems'
-# require 'bundler'
-# Bundler.require
 
 Motion::Project::App.setup do |app|
   # Use `rake config' to see complete project settings.
   app.delegate_class = 'FakeDelegate'
-  app.files = (app.files.select { |f| f =~ /app\/lib/ } + app.files).uniq
+  app.files = Dir.glob('./lib/motion_model/**/*.rb')
+  app.files = (Dir.glob('./app/**/*.rb') + app.files).uniq
 end
--- lib/motion_model.rb
+++ lib/motion_model.rb
@@ -3,15 +3,3 @@ Motion::Project::App.setup do |app|
     app.files.unshift(file)
   end
 end
-
-# Dir[File.join(File.dirname(__FILE__), 'nitron/data/model/**/*.rb')].each { |file| app.files.unshift(file) }
-# Dir[File.join(File.dirname(__FILE__), 'nitron/data/relation/**/*.rb')].each { |file| app.files.unshift(file) }
-# 
-# app.files.unshift(File.join(File.dirname(__FILE__), 'nitron/view_controller.rb'))
-# app.files.unshift(File.join(File.dirname(__FILE__), 'nitron/ui/data_binding_support.rb'))
-# app.files.unshift(File.join(File.dirname(__FILE__), 'nitron/ui/outlet_support.rb'))
-# app.files.unshift(File.join(File.dirname(__FILE__), 'nitron/ui/action_support.rb'))
-# 
-# 
-# 
-# %w(ext model validatable input_helpers).each{|file| require File.join(File.dirname(__FILE__), "app/lib/#{file}.rb")}
