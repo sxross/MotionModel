3cec999f79fbe5ff99835bc448e7fec0061080a3
Steve Ross <sxross@gmail.com>
2012-12-30 14:04:48 -0800

    Added spec for supporting column options -- needed for formotion support

---
+++ spec/column_options_spec.rb
@@ -0,0 +1,23 @@
+class ModelWithOptions
+  include MotionModel::Model
+
+  columns :date => {:type => :date, :formotion => {:picker_type => :date_time}}
+end
+
+describe "column options" do
+  it "accepts the hash form of column declaration" do
+    lambda{ModelWithOptions.new}.should.not.raise
+  end
+
+  it "retrieves non-nil options for a column declaration" do
+    instance = ModelWithOptions.new
+    instance.options(:date).should.not.be.nil
+  end
+
+  it "retrieves correct options for a column declaration" do
+    instance = ModelWithOptions.new
+    puts instance.options(:date)
+    instance.options(:date)[:formotion].should.not.be.nil
+    instance.options(:date)[:formotion][:picker_type].should == :date_time
+  end
+end
