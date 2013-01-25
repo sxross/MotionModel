4e3fa394e32c4fb3285411b6eeaa24cf8acc1889
Steve Ross <sxross@gmail.com>
2013-01-25 12:51:18 -0800

    Added transactions to allow for undo if a user cancels an edit.

---
+++ lib/motion_model/model/transaction.rb
@@ -0,0 +1,43 @@
+module MotionModel
+  module Model
+    module Transactions
+      def transaction(&block)
+        if block_given?
+          @savepoints = [] if @savepoints.nil?
+          @savepoints.push self.duplicate
+          yield
+          @savepoints.pop
+        else
+          raise ArgumentError.new("transaction must have a block")
+        end
+      end
+
+      def rollback
+        unless @savepoints.empty?
+          restore_attributes
+        else
+          NSLog "No savepoint, so rollback not performed."
+        end
+      end
+
+      def columns_without_relations
+        columns.select{|col| ![:has_many, :belongs_to].include?(type(col))}
+      end
+
+      def restore_attributes
+        savepoint = @savepoints.last
+        if savepoint.nil?
+          NSLog "No savepoint, so rollback not performed."
+        else
+          columns_without_relations.each do |col|
+            self.send("#{col}=", savepoint.send(col))
+          end
+        end
+      end
+
+      def duplicate
+        Marshal.load(Marshal.dump(self))
+      end
+    end
+  end
+end
---
+++ spec/transaction_spec.rb
@@ -0,0 +1,91 @@
+class TransactClass
+  include MotionModel::Model
+  include MotionModel::Model::Transactions
+  columns :name, :age
+  has_many :transaction_things
+end
+
+class TransactionThing
+  include MotionModel::Model
+  include MotionModel::Model::Transactions
+  columns :thingie_description
+  belongs_to :transact_class
+end
+
+describe "transactions" do
+  before{TransactClass.destroy_all}
+
+  it "wraps a transaction but auto-commits" do
+    item = TransactClass.create(:name => 'joe', :age => 22)
+    item.transaction do
+      item.name = 'Bob'
+    end
+    item.name.should == 'Bob'
+    TransactClass.find(:name).eq('Bob').count.should == 1
+  end
+
+  it "wraps a transaction but can rollback to a savepoint" do
+    item = TransactClass.create(:name => 'joe', :age => 22)
+    item.transaction do
+      item.name = 'Bob'
+      item.rollback
+    end
+    item.name.should == 'joe'
+    TransactClass.find(:name).eq('joe').count.should == 1
+    TransactClass.find(:name).eq('Bob').count.should == 0
+  end
+
+  it "allows multiple savepoints -- inside one not exercised" do
+    item = TransactClass.create(:name => 'joe', :age => 22)
+    item.transaction do
+      item.transaction do
+        item.name = 'Bob'
+      end
+      item.rollback
+      item.name.should == 'joe'
+      TransactClass.find(:name).eq('joe').count.should == 1
+      TransactClass.find(:name).eq('Bob').count.should == 0
+    end
+   end
+
+  it "allows multiple savepoints -- inside one exercised" do
+    item = TransactClass.create(:name => 'joe', :age => 22)
+    item.transaction do
+      item.transaction do
+        item.name = 'Ralph'
+        item.rollback
+      end
+      item.name.should == 'joe'
+      TransactClass.find(:name).eq('joe').count.should == 1
+      TransactClass.find(:name).eq('Bob').count.should == 0
+    end
+   end
+
+  it "allows multiple savepoints -- set in outside context rollback in inside" do
+    item = TransactClass.create(:name => 'joe', :age => 22)
+    item.transaction do
+      item.name = 'Ralph'
+      item.transaction do
+        item.rollback
+      end
+      item.name.should == 'Ralph'
+      TransactClass.find(:name).eq('Ralph').count.should == 1
+    end
+   end
+
+  it "allows multiple savepoints -- multiple savepoints exercised" do
+    item = TransactClass.create(:name => 'joe', :age => 22)
+    item.transaction do
+      item.name = 'Ralph'
+      item.transaction do
+        item.name = 'Paul'
+        item.rollback
+        item.name.should == 'Ralph'
+        TransactClass.find(:name).eq('Ralph').count.should == 1
+      end
+      item.rollback
+      item.name.should == 'joe'
+      TransactClass.find(:name).eq('joe').count.should == 1
+    end
+   end
+end
