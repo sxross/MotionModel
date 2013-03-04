b5bd00c51557084246287ca55749e4b7d894f99a
Doug Puchalski <doug316@gmail.com>
2013-03-04 12:18:00 -0800

    Revert from FMDB queue to db, use semaphors for multi-thread support.
    FMDB Queues did not provide the required visibility to handle transactions whiel other threads might be running as well. When a transaction is pending, this prevents other requests from getting in. We use our own GCD queue for DB accesses.

--- lib/motion_model/adapters/sql/fmdb_adapter.rb
+++ lib/motion_model/adapters/sql/fmdb_adapter.rb
@@ -11,26 +11,9 @@ module MotionModel
         @depth = 0
       end
 
-      def begin_transaction
-        @pending = true
-        @db.beginTransaction
-      end
-
-      def commit
-        return unless @pending
-        @db.commit
-        @pending = false
-      end
-
-      def rollback
-        return unless @pending
-        @db.rollback
-        @pending = false
-        @rolling_back = true
-      end
-
       def execute(&block)
         return if @rolling_back
+        result = nil
         if @pending
           result = block.call(@db)
         else
@@ -40,10 +23,56 @@ module MotionModel
             commit
           rescue => exc
             rollback
+            raise
           end
         end
         result
       end
+
+      def self.execute(&block)
+        raise 'No transaction pending' unless pending?
+        pending_transaction.execute(&block)
+      end
+
+      def self.pending?
+        !!pending_transaction
+      end
+
+      def self.pending_transaction
+        NSThread.currentThread.threadDictionary['fmdb_pending_transaction']
+      end
+
+      def self.pending_transaction=(transaction)
+        raise 'Transaction already pending' if transaction && self.pending?
+        NSThread.currentThread.threadDictionary['fmdb_pending_transaction'] = transaction
+      end
+
+      private
+
+      def begin_transaction
+        self.class.pending_transaction = self
+        @pending = true
+        @db.beginTransaction
+      end
+
+      def end_transaction
+        @pending = false
+        self.class.pending_transaction = nil
+      end
+
+      def commit
+        return unless @pending
+        @db.commit
+        end_transaction
+      end
+
+      def rollback
+        return unless @pending
+        @db.rollback
+        end_transaction
+        @rolling_back = true
+      end
+
     end
 
     def initialize(name = nil, options = {})
@@ -58,24 +87,36 @@ module MotionModel
       File.join(NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, true)[0], @name)
     end
 
-    def queue
-      if @queue.nil?
-        @queue = FMDatabaseQueue.databaseQueueWithPath(db_path)
+    def db
+      unless @db
+        @db = FMDatabase.databaseWithPath(db_path)
+        @db.crashOnErrors = true
+        @db.open
       end
-      @queue
+      @db
     end
 
     def execute_sql(sql)
-      result = nil
-      if @transaction
-        @transaction.execute do |db|
-          result = self.class._execute(db, sql)
-        end
-      else
-        queue.inDatabase( -> (db) {
-          result = self.class._execute(db, sql)
-        } )
+      block = proc do
+        self.class._execute(db, sql)
       end
+
+      result = nil
+      begin
+        @result = nil
+        if Transaction.pending?
+          result = Transaction.execute(&block)
+        else
+          result_semaphore.wait
+          begin
+            queue.sync { @result = block.call }
+            result = @result
+            @result = nil
+          end
+          result_semaphore.signal
+        end
+      end
+
       result
     end
 
@@ -98,15 +139,27 @@ module MotionModel
 
     # TODO IMPORTANT need to test transactions and rollbacks
     def transaction(&block)
-      if @transaction
-        @transaction.execute(&block)
-      else
-        queue.inDatabase( -> (db) {
-          @transaction ||= Transaction.new(db)
-          @transaction.execute(&block)
-          @transaction = nil
-        })
+      begin
+        if Transaction.pending?
+          Transaction.execute(&block)
+        else
+          queue.sync do
+            Transaction.new(db).execute(&block)
+          end
+        end
       end
+    end
+
+    def queue
+      unless @queue
+        queue_name = "#{NSBundle.mainBundle.bundleIdentifier}.#{self.class.name.underscore}"
+        @queue = ::Dispatch::Queue.new(queue_name)
+      end
+      @queue
+    end
+
+    def result_semaphore
+      @result_semaphore ||= Dispatch::Semaphore.new(1)
     end
 
   end
