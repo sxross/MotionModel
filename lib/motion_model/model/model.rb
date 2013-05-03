f6cea1d1cc40781a7baad5de3e43129b38392d90
Doug Puchalski <doug316@gmail.com>
2013-05-03 12:11:28 -0700

    Remove redundant calls to #column

--- lib/motion_model/model/model.rb
+++ lib/motion_model/model/model.rb
@@ -362,13 +362,13 @@ module MotionModel
       # All relation columns, including type and id columns for polymorphic associations
       def relation_column?(col) #nodoc
         _col = column(col)
-        [:belongs_to, :belongs_to_id, :belongs_to_type, :has_many, :has_one].include? column(_col).type
+        [:belongs_to, :belongs_to_id, :belongs_to_type, :has_many, :has_one].include?(_col.type)
       end
 
       # Polymorphic association columns that are not stored in DB
       def virtual_polymorphic_relation_column?(col) #nodoc
         _col = column(col)
-        [:belongs_to, :has_many, :has_one].include? column(_col).type
+        [:belongs_to, :has_many, :has_one].include?(_col.type)
       end
 
       def has_relation?(col) #nodoc
