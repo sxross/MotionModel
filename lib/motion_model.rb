58fcebef18adfc46e0ffdcb27e1c52bb6c33818d
Felipe <solanoluz@gmail.com>
2014-01-30 10:03:02 -0200

    fixed dependency issue on sql_adapters

--- lib/motion_model.rb
+++ lib/motion_model.rb
@@ -1,6 +1,3 @@
-require 'motion-require'
-require 'motion-support'
-
-%w(*.rb model/*.rb adapters/*.rb adapters/sql/*.rb).each do |path|
+%w(*.rb model/*.rb adapters/*.rb adapters/sql/sql_db_adapter.rb adapters/sql/sqlite3_adapter.rb adapters/sql/*.rb).each do |path|
   Motion::Require.all(Dir.glob(File.expand_path("../../motion/#{path}", __FILE__)))
 end
