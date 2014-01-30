b0092bbddaf5b1848e8aa45a48efbf916c805885
Felipe <solanoluz@gmail.com>
2014-01-30 08:38:47 -0200

    update require paths

--- lib/motion_model.rb
+++ lib/motion_model.rb
@@ -1,6 +1,6 @@
 require 'motion-require'
 require 'motion-support'
 
-%w(*.rb model/*.rb adapters/*.rb).each do |path|
+%w(*.rb model/*.rb adapters/*.rb adapters/sql/*.rb).each do |path|
   Motion::Require.all(Dir.glob(File.expand_path("../../motion/#{path}", __FILE__)))
 end
