a719c9a167bc39b7c392c257a3379daaa59b835e
Luis Felipe Colle da Luz <solanoluz@gmail.com>
2014-08-04 14:33:35 +0100

    rtyng to fix dependencies issue

--- Rakefile
+++ Rakefile
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
-require "bundler/gem_tasks"
 $:.unshift("/Library/RubyMotion/lib")
 require 'motion/project/template/ios'
-require 'bundler'
-Bundler.require(:default)
+
+begin
+  require 'bundler'
+  Bundler.require
+rescue LoadError
+end
 
 require 'motion-cocoapods'
-
-require 'motion_model'
-require 'motion_model/array'
-require 'motion_model/sql'
 
 $: << File.expand_path('/lib', __FILE__)
 
@@ -19,9 +18,8 @@ Motion::Project::App.setup do |app|
   # Use `rake config' to see complete project settings.
   app.name = 'MotionModel'
   app.delegate_class = 'FakeDelegate'
-  app.detect_dependencies = false
 
   app.pods do
-    pod 'FMDB', '2.1'
+    pod 'FMDB', '2.0'
   end
 end
--- lib/motion_model.rb
+++ lib/motion_model.rb
@@ -1,12 +1,4 @@
-# Motion::Require.all(Dir.glob(File.expand_path("../../motion/#{path}", __FILE__)))
-# %w[*.rb model/*.rb adapters/*.rb adapters/array/*.rb adapters/sql/sql_db_adapter.rb adapters/sql/sqlite3_adapter.rb adapters/sql/*.rb].each do |path|
-#   Motion::Require.all(Dir.glob(File.expand_path("../../motion/#{path}", __FILE__)))
-# end
-
-
-require 'motion-require'
-require 'motion-support'
-
-%w(*.rb model/*.rb adapters/*.rb).each do |path|
+Motion::Require.all(Dir.glob(File.expand_path("../../motion/#{path}", __FILE__)))
+%w[*.rb model/*.rb adapters/*.rb adapters/array/*.rb adapters/sql/sql_db_adapter.rb adapters/sql/sqlite3_adapter.rb adapters/sql/*.rb].each do |path|
   Motion::Require.all(Dir.glob(File.expand_path("../../motion/#{path}", __FILE__)))
 end
